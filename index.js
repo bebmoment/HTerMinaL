f5cdd256e5b9135d9a4df04bc91aee996e4eaeb3
Ben Wang <benwangott@gmail.com>
2025-07-21 18:07:08 -0400

    refactoring, use wrapper function, & remove node-bash

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 /* 
 server
-TODO: write helper functions for rendering stuff because the posts are getting unwieldly
 */
+let DANGEROUS_MODE = false; // silly global state that i'm trying to get rid of
 
-// SET TO TRUE ONLY IF YOU WANT BOZOS RUNNING RANDOM SHELL COMMANDS ON YOUR PC
-let DANGEROUS_MODE = false;
 // imports and initializations
 import express from 'express';
 import { exec } from 'child_process';
 import bodyParser from 'body-parser'; 
-import { Bash } from 'node-bash'; // might replace with exec
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 
@@ -23,47 +20,27 @@ app.use(express.static(`${__dirname}/public`)); // the one thing that makes css 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded( {extended: false} ));
 
-// initial render
-// app.get('/', (req, res) => {
-// 	res.render('index.ejs', {output: "", DANGEROUS_MODE: DANGEROUS_MODE}) 
-// });
-
 app.get('/', (req, res) => {
 	bebRender(req, res, 'index.ejs', "", DANGEROUS_MODE);
 })
 app.get('/hack', (req, res) => {
-	res.render('hack.ejs', {result: ""})
+	res.render('hack.ejs', {output: ""})
 });
 
-// start message
-app.listen(port, () => {
-	console.log(`app listening on port ${port}`);
-});
 app.post('/hack', (req, res) => {
 	exec(`./hack ${req.body.login}`, (err, stdout, stderr) => {
-		if(err) {
-			res.render('hack.ejs', {result: stderr})
-		} else {
-			DANGEROUS_MODE = true;
-			res.redirect('/')
-		}
+		bebRender(req, res, err ? 'hack.ejs' : 'index.ejs', err ? stderr : stdout, !err)
+		DANGEROUS_MODE = true;
 	})
 });
 
 // handling command
+// trying to remove state dependence is a bit difficult
 app.post('/', (req, res) => {
-	if (!DANGEROUS_MODE) {
-		const ball = bashEcho(req.body.cmd);
-		ball.then((x) => res.render('index.ejs', { output: `${x.raw}`, DANGEROUS_MODE: DANGEROUS_MODE }));
-	} else {
-		exec(req.body.cmd, (err, stdout, stderr) => { // this is a callback btw: that's why you couldn't make a return properly
-			if (err) {
-				res.render('index.ejs', {output: stderr.toString(), DANGEROUS_MODE: DANGEROUS_MODE});
-			} else {
-				res.render('index.ejs', {output: stdout.toString(), DANGEROUS_MODE: DANGEROUS_MODE});
-			}
-		})
-	}
+	// const line = (DANGEROUS_MODE) ? req.body.cmd : `echo ${req.body.cmd}`
+	exec((DANGEROUS_MODE) ? req.body.cmd : `echo ${req.body.cmd}`, (err, stdout, stderr) => { // no way this is it
+		bebRender(req, res, 'index.ejs', err ? stderr : stdout, DANGEROUS_MODE);
+	})
 });
 
 app.put('/', (req, res) => {
@@ -71,12 +48,10 @@ app.put('/', (req, res) => {
 	res.redirect('/');
 })
 
-// might replace with an exec call soon
-function bashEcho(what) {
-	return Bash.$`echo ${what}`;
-}
-// starting wrapper functions
-// might have to curry heehoo
-function bebRender(req, res, page, out, danger) {
+app.listen(port, () => {
+	console.log(`app listening on port ${port}`);
+});
+
+function bebRender(req, res, page, out, danger) { // TODO: possible return type to indicate dangerous mode
 	res.render(page, {output: out, DANGEROUS_MODE: danger})
 }
